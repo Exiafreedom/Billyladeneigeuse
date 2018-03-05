804c859b68b76b45617c9d04c944fa2deca575b3
SztrojKing <ga8hell@gmail.com>
2018-03-05 15:33:57 +0100

    Python Robot arret sensor

---
+++ main.py
@@ -0,0 +1,17 @@
+from megapi import *
+
+if __name__ == '__main__':
+	bot = MegaPi()
+	bot.start()
+	bot.motorRun(M1,0);
+	sleep(1);
+	while 1:
+		sleep(1);
+		bot.motorRun(M1,50);
+		sleep(1);
+		bot.motorRun(M1,0);
+		sleep(1);
+		bot.motorRun(M2,-50);
+		sleep(1);
+		bot.motorRun(M2,0);
+
